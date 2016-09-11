e54c44948455b2f941f7c972c1397e5ab828c107
jimsorock <jimmy.muga@gmail.com>
2016-09-11 11:09:46 -0700

    scripts

---
+++ Assets/Scripts/FlickerLights.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlickerLights : MonoBehaviour
+{
+
+    public float startFlicker = 15f;
+    public float shutoff = 25f;
+    private Light lite;
+
+    void Start()
+    {
+        lite = this.GetComponent<Light>();
+        StartCoroutine(LateCall());
+    }
+
+    IEnumerator LateCall()
+    {
+        yield return new WaitForSeconds(startFlicker);
+        lite.enabled = !lite.enabled;
+        yield return new WaitForSeconds(Random.Range(0.1f, 0.3f));
+        lite.enabled = !lite.enabled;
+        yield return new WaitForSeconds(Random.Range(0.1f, 0.5f));
+        lite.enabled = !lite.enabled;
+        yield return new WaitForSeconds(Random.Range(0.1f, 0.5f));
+        lite.enabled = !lite.enabled;
+        yield return new WaitForSeconds(shutoff);
+        lite.enabled = false;
+    }
+}
---
+++ Assets/Scripts/ShowCrawler.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowCrawler : MonoBehaviour
+{
+
+
+    public float show = 25f;
+
+    void Start()
+    {
+        StartCoroutine(ActivateRoutine());
+    }
+    private IEnumerator ActivateRoutine()
+    {
+        yield return new WaitForSeconds(show);    // and now we wait !
+
+        // make a list of all children
+        Transform[] ChildrenTransforms = this.gameObject.GetComponentsInChildren<Transform>(true);
+        foreach (Transform t in ChildrenTransforms)
+            t.gameObject.SetActive(true);      // enable all the objects in the list (even the parent)
+    }
+}
